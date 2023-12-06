c32fca0ac4a6e449f6e74dbf3abdd1cac214558a
script-app <153062744+script-app@users.noreply.github.com>
2023-12-06 18:50:01 +0700

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,9 @@
+ window.onload = ()=>{
+      Swal.fire({
+  position: "center",
+  icon: "success",
+  title: "ยินดีต้อนรับเข้าสู่เว็บไซต์",
+  showConfirmButton: false,
+  timer: 1500
+});
+      }
